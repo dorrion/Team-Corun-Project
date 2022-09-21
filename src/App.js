8ade315a041d8fa8eeddfc3b851d9ff5217f3f84
hun0613 <pshun7614@gmail.com>
2022-09-22 00:42:10 +0900

    TCP-10 프로젝트 디렉토리 설정

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import { ThemeProvider } from 'styled-components';
+import theme from "./data/theme";
+
 
 function App() {
   return (
-    <>
-    </>
+    <ThemeProvider theme = {theme}>
+      
+    </ThemeProvider>
   );
 }
 
---
+++ src/data/theme.js
@@ -0,0 +1,7 @@
+const theme = {
+    black : '#000000',
+    gray : '#DCDCDC',
+    bgColor : '#FAFAFA'
+}
+
+export default theme;
